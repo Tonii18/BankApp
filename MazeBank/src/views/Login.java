54019a5997dc4db87773d251104e7dd761f8cf53
Tonii18 <ajsanrosal@gmail.com>
2024-10-03 18:54:50 +0200

    Inicio del Login, creacion de los paneles

--- MazeBank/src/views/Login.java
+++ MazeBank/src/views/Login.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
 
 public class Login extends JFrame {
 
@@ -31,12 +32,32 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1280, 720);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLocationRelativeTo(null);
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel firstSide = new JPanel();
+		firstSide.setBackground(new Color(61, 60, 60));
+		firstSide.setBounds(0, 0, 640, 683);
+		contentPane.add(firstSide);
+		firstSide.setLayout(null);
+		
+		JPanel loginPanel = new JPanel();
+		loginPanel.setBounds(107, 55, 425, 573);
+		firstSide.add(loginPanel);
+		
+		JPanel secondSide = new JPanel();
+		secondSide.setBackground(new Color(44, 144, 151));
+		secondSide.setBounds(640, 0, 626, 683);
+		contentPane.add(secondSide);
+		
+		
+		
 	}
-
 }
